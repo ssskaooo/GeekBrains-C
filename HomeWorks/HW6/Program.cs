d87ec7cc097570ce8e1bdd52f64ea42ba96a0350
ssskaooo <91846474+ssskaooo@users.noreply.github.com>
2023-10-07 13:04:37 +0300

    added Home Work 6

--- HomeWorks/HW6/Program.cs
+++ HomeWorks/HW6/Program.cs
@@ -5,6 +5,36 @@
 
 // Решение
 
+int CheckPositiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] InputNumber(int m)
+{
+    int[] array = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        System.Console.Write("Введите целое число: ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+System.Console.WriteLine("Введите колличество целых чисел: ");
+int m = int.Parse(Console.ReadLine());
+int[] array = InputNumber(m);
+System.Console.WriteLine($"Вы ввели {CheckPositiveNumber(array)} положительных числа(чисел)");
+
+
 
 
 
@@ -15,3 +45,45 @@
 
 // Решение
 
+double X(double k1, double k2, double b1, double b2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+double Y(double x, double k1, double b1)
+{
+    return k1 * x + b1;
+}
+
+double inputDouble(string promt)
+{
+    Console.Write(promt);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+bool ValidateLines(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2)
+    {
+        if (b1 == b2)
+            Console.WriteLine("Прямые совдадают");
+        else
+            Console.WriteLine("Прямые параллельны");
+
+        return false;
+    }
+    return true;
+}
+
+Console.WriteLine("Для уравнений вида y=k*x+b");
+double k1 = inputDouble("Введите к1: ");
+double b1 = inputDouble("Введите b1: ");
+double k2 = inputDouble("Введите к2: ");
+double b2 = inputDouble("Введите b2: ");
+if (ValidateLines(k1, b1, k2, b2))
+{
+    double x = X(k1, k2, b1, b2);
+    double y = Y(x, k1, b1);
+    Console.WriteLine($"Для уравнений y={k1}*x+{b1} и y={k2}*x+{b2}");
+    Console.WriteLine($"Общая точка имеет координаты ({x}; {y})");
+}
