cbe2c74e85c33de84ab6abddcb5e1b5a92966e8e
ssskaooo <91846474+ssskaooo@users.noreply.github.com>
2023-10-18 12:51:44 +0300

    added Home Work 9

--- HomeWorks/HW9/Program.cs
+++ HomeWorks/HW9/Program.cs
@@ -3,7 +3,16 @@
 //N = 5 -> "5, 4, 3, 2, 1"
 //N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+//void PrintN(int n)
+//{
+//    if (n > 0)
+//    {
+//        System.Console.Write($"{n} ");
+//        PrintN(n - 1);
+//    }
+//}
 
+//PrintN(10);
 
 
 
@@ -11,6 +20,29 @@
 //элементов в промежутке от M до N.Выполнить с помощью рекурсии.
 //M = 1; N = 15-> 120
 //M = 4; N = 8. -> 30
+
+//using System;
+
+//int GetSumRange(int m, int n)
+//{
+//    int min = m > n ? n : m;
+//    int max = m > n ? m : n;
+//    if (min < max)
+//    {
+//        return GetSumRange(min, max - 1) + max;
+//    }
+//    else if (min == max)
+//        return min;
+//    else
+//        return 0;
+//}
+
+//int m = 1;
+//int n = 5;
+//System.Console.WriteLine($"sum({m};{n}) = {GetSumRange(m, n)}");
+//m = 5;
+//n = 1;
+//System.Console.WriteLine($"sum({m};{n}) = {GetSumRange(m, n)}");
 
 
 
@@ -22,6 +54,24 @@
 //m = 3, n = 2->A(m, n) = 29
 
 
+
+
+int AkkermanFunction(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else if (m == 0)
+        return AkkermanFunction(n - 1, 1);
+    else
+        return AkkermanFunction(n - 1, AkkermanFunction(n, m - 1));
+}
+
+int n = 2;
+int m = 3;
+System.Console.WriteLine($"Ackerman({n},{m})= {AkkermanFunction(n, m)}");
+n = 3;
+m = 2;
+System.Console.WriteLine($"Ackerman({n},{m})= {AkkermanFunction(n, m)}");
 
 
 
--- Seminars/Sem9/Program.cs
+++ Seminars/Sem9/Program.cs
@@ -4,92 +4,65 @@
 //N = 6-> "1, 2, 3, 4, 5, 6"
 
 
-//void ShowNums(int n) 
-//{ 
-//    if(n > 0)
-//    {
-//        ShowNums(n - 1);
-//        Console.Write(n + " ");
-//    }
-//}
-//ShowNums(10);
+void ShowNums(int n) 
+{
+    if (n > 0)
+    {
+        ShowNums(n - 1);
+        Console.Write(n + " ");
+    }
+}
+ShowNums(10);
 
 
 //Задание 2. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
 //453 -> 12
 //45 -> 9
 
-//int FindSum(int num)
-//{
-//    if(num > 0) return  FindSum(num / 10) + num % 10;
-//    else return num;
+int FindSum(int num)
+{
+    if (num > 0) return FindSum(num / 10) + num % 10;
+    else return num;
 
-//}
+}
 
-//Console.WriteLine(FindSum(453)); 
+Console.WriteLine(FindSum(453));
 
 //Задача 3. Задайте значения M и N. Напишите программу, которая выведет все
 //натуральные числа в промежутке от M до N.
 //M = 1; N = 5. -> "1, 2, 3, 4, 5"
 //M = 4; N = 8. -> "4, 5, 6, 7, 8"
 
-//using System;
+void PrintRange(int m, int n)
+{
+    int min = m > n ? n : m;
+    int max = m > n ? m : n;
+    if (min < max)
+    {
+        PrintRange(min, max - 1);
+        System.Console.Write($"{max} ");
+    }
+    else if (min == max)
+        System.Console.Write($"{min} ");
+}
 
-//class Program
-//{
-//    static void Main()
-//    {
-//        Console.Write("Введите значение M: ");
-//        int m = int.Parse(Console.ReadLine());
-
-//        Console.Write("Введите значение N: ");
-//        int n = int.Parse(Console.ReadLine());
-
-//        Console.WriteLine("Натуральные числа в промежутке от M до N: ");
-//        PrintNumbers(m, n);
-//    }
-
-//    static void PrintNumbers(int m, int n)
-//    {
-//        if (m <= n)
-//        {
-//            Console.WriteLine(m);
-//            PrintNumbers(m + 1, n);
-//        }
-//    }
-//}
+PrintRange(23, 1);
 
 
 
 //Задача 4.Напишите программу, которая на вход принимает два числа A и B, и
 //возводит число А в целую степень B с помощью рекурсии.
-//A = 3; B = 5 -> 243 (3⁵)
-//A = 2; B = 3 -> 8
+//A = 3; B = 5-> 243(3⁵)
+//A = 2; B = 3-> 8
 
 
-//class Program
-//{
-//    static void Main()
-//    {
-//        Console.Write("Введите число A: ");
-//        int a = Convert.ToInt32(Console.ReadLine());
+int GetPower(int base_, int exponent)
+{
+    if (exponent == 1)
+        return base_;
+    return GetPower(base_, exponent - 1) * base_;
+}
 
-//        Console.Write("Введите степень B: ");
-//        int b = Convert.ToInt32(Console.ReadLine());
-
-//        int result = Power(a, b);
-//        Console.WriteLine("Результат: " + result);
-//    }
-
-//    static int Power(int a, int b)
-//    {
-//        if (b == 0)
-//        {
-//            return 1;
-//        }
-//        else
-//        {
-//            return a * Power(a, b - 1);
-//        }
-//    }
-//}
+int base_ = 3;
+int exponent = 3;
+System.Console.WriteLine($"{base_}^{exponent} = {GetPower(base_, exponent)}");
